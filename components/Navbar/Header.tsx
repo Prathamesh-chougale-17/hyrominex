8ff2b2794b5d063bc53b1ea96467cb95036f040e
Prathamesh-chougale-17 <prathamesh17170@gmail.com>
2023-12-12 11:13:51 +0530

    Refactor page.tsx and Header.tsx

--- components/Navbar/Header.tsx
+++ components/Navbar/Header.tsx
@@ -1,18 +1,14 @@
 "use client";
-import { Autocomplete, Group, Burger, rem } from "@mantine/core";
-import { useDisclosure } from "@mantine/hooks";
-// import { IconSearch } from "@tabler/icons-react";
-// import { MantineLogo } from "@mantinex/mantine-logo";
+import { Group, Burger } from "@mantine/core";
 import MantineLogo from "../../public/android-chrome-192x192.png";
 import classes from "./Header.module.css";
 import Image from "next/image";
 import { useState } from "react";
 
 const links = [
-  { link: "/about", label: "Features" },
-  { link: "/pricing", label: "Pricing" },
-  { link: "/learn", label: "Learn" },
-  { link: "/community", label: "Community" },
+  { link: "/", label: "Home" },
+  { link: "/maps", label: "Maps" },
+  { link: "/weather", label: "Weather" },
 ];
 
 const Header = () => {
@@ -47,26 +43,6 @@ const Header = () => {
           <Group ml={50} gap={5} className={classes.links} visibleFrom="sm">
             {items}
           </Group>
-          {/* <Autocomplete
-            className={classes.search}
-            placeholder="Search"
-            leftSection={
-              <IconSearch
-                style={{ width: rem(16), height: rem(16) }}
-                stroke={1.5}
-              />
-            }
-            data={[
-              "React",
-              "Angular",
-              "Vue",
-              "Next.js",
-              "Riot.js",
-              "Svelte",
-              "Blitz.js",
-            ]}
-            visibleFrom="xs"
-          /> */}
         </Group>
       </div>
     </header>
