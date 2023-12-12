5ead87dd718e7a32ea4d2f288e3386317fe07e62
Prathamesh-chougale-17 <prathamesh17170@gmail.com>
2023-12-12 11:37:37 +0530

    Update metadata and responsive styles in layout and footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,10 +4,30 @@ import "@mantine/core/styles.css";
 import { MantineProvider, ColorSchemeScript } from "@mantine/core";
 import Header from "@/components/Navbar/Header";
 import { FooterLinks } from "@/components/Footer/Footer";
+import { Metadata } from "next";
 
-export const metadata = {
-  title: "My Mantine app",
-  description: "I have followed setup instructions carefully",
+export const metadata: Metadata = {
+  title: "Hydrominex",
+  description: "Made by Team Hydrominex",
+  icons:
+    "https://img.freepik.com/premium-vector/dump-truck-icon-isolated-white-background_103044-862.jpg?w=740",
+  metadataBase: new URL("https://hydrominex.vercel.app/"),
+  openGraph: {
+    title: "Hydrominex",
+    description: "Hydrominex's personal website",
+    url: "https://hydrominex.vercel.app/",
+    siteName: "Hydrominex",
+    images: [
+      {
+        url: "https://img.freepik.com/premium-vector/dump-truck-icon-isolated-white-background_103044-862.jpg?w=740",
+        width: 800,
+        height: 600,
+        alt: "Hydrominex",
+      },
+    ],
+    locale: "en_IE",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
