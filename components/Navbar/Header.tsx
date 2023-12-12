d4e52d0d4f965f6b6afe8979d3d6bbfd0f921c2a
Prathamesh-chougale-17 <prathamesh17170@gmail.com>
2023-12-12 16:27:36 +0530

    Update DarkMode and Navbar components

--- components/Navbar/Header.tsx
+++ components/Navbar/Header.tsx
@@ -24,16 +24,57 @@ const Header = () => {
       <div className={classes.inner}>
         <Group>
           {/* <MantineLogo size={28} /> */}
-          <Image src={MantineLogo} alt="Mantine Logo" width={60} height={60} />
+          <Link href="/">
+            <Image
+              src={MantineLogo}
+              alt="HyroMinex Logo"
+              width={60}
+              height={60}
+              className={classes.logo}
+              priority
+            />
+          </Link>
         </Group>
 
         <Group>
-          <Burger
-            opened={opened}
-            onClick={() => setOpened(!opened)}
-            size="sm"
-            hiddenFrom="sm"
-          />
+          <Group hiddenFrom="sm">
+            <Menu>
+              <Menu.Target>
+                <Burger
+                  opened={opened}
+                  onClick={() => setOpened(!opened)}
+                  size="sm"
+                  hiddenFrom="sm"
+                />
+              </Menu.Target>
+              <Menu.Dropdown>
+                {links.map((link) => (
+                  <Menu.Item key={link.label}>
+                    <Link className={classes.Dropdown} href={link.link}>
+                      {link.label}
+                    </Link>
+                  </Menu.Item>
+                ))}
+              </Menu.Dropdown>
+            </Menu>
+            <ActionToggle />
+            {status === "authenticated" ? (
+              <Menu>
+                <Menu.Target>
+                  <Avatar src={image} className={classes.Avatar} />
+                </Menu.Target>
+                <Menu.Dropdown>
+                  <Link href="/api/auth/signout">
+                    <Button>Sign out</Button>
+                  </Link>
+                </Menu.Dropdown>
+              </Menu>
+            ) : (
+              <Link href="/api/auth/signin">
+                <Button fullWidth>Sign in</Button>
+              </Link>
+            )}
+          </Group>
           <Group ml={50} gap={5} className={classes.links} visibleFrom="sm">
             {links.map((link) => (
               <Link key={link.label} href={link.link} className={classes.link}>
